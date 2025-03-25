46782cd2bc20bd80961708c39eb4aa33b68dd9cc
prachipandey <pandeyprachi8572@gmail.com>
2025-03-26 00:29:10 +0530

    portfolio

---
+++ src/components/projects/Projects.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import Title from '../layouts/Title'
+import { projectOne, projectTwo, projectThree , projectFour,projectFive,projectSix } from "../../assets/index";
+import ProjectsCard from './ProjectsCard';
+const Projects = () => {
+  return (
+    <section
+      id="projects"
+      className="w-full py-20 border-b-[1px] border-b-black"
+    >
+      <div className="flex justify-center items-center text-center">
+        <Title
+          title="VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK"
+          des="My Projects"
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
+        <ProjectsCard
+          title = "Quiz App"
+          des="  I have created a quiz app using HTML , CSS and  JavaScript  with responsive all features !"
+          src={projectOne}
+        />
+        <ProjectsCard
+          title="Weather App"
+          des= "I have created a weather app using HTML , CSS ,  JavaScript and Bootstrap. The app is fully responsive and utilizes a weather API."
+          src={projectTwo}
+        />
+        <ProjectsCard
+          title="Food Items "
+          des=" I have created a Food Items using HTML , CSS , React and tailwind css . The Food Items is fully responsive and when click to add button and then add tha lower box add the food items !"
+          src={projectThree}
+        />
+        <ProjectsCard
+          title="Wall Clock "
+          des="I have created a Wall Clock using HTML , CSS , and  Javascript , and Boostrap . the clock is fully responsive !"
+          src={projectFour}
+        />
+        <ProjectsCard
+          title=" Company Employee Web Page"
+          des=" I have created a Company employee web page  using HTML , CSS ,and  Javascript , Boostrap . the company employee web page is fully responsive "
+          src={projectFive}
+        />
+        <ProjectsCard
+          title=" Digital Snug"
+          des=" I have created a Digital Snug using HTML , CSS ,and  Javascript , Boostrap . the Digital Snug  web page is fully responsive "
+          src={projectSix}
+        />
+      </div>
+    </section>
+  );
+}
+export default Projects
---
+++ src/components/resume/Education.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+import ResumeCard from './ResumeCard';
+const Education = () => {
+return (
+<motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { duration: 0.5 } }}
+      className="w-full flex flex-col lgl:flex-row gap-10 lgl:gap-20">
+      {/* part one */}
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2019 - 2024</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Education Quality</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="BCA (Batchlor of computer science)"
+            subTitle="University of Prayagraj (2021 - 2024)"
+            result="68.6%"
+            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+          />
+          
+          <ResumeCard
+            title="Science "
+            subTitle="National Inter College (2019 - 2021)"
+            result="71.6%"
+            des=" education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+          />
+          <ResumeCard
+            title="Secondary School Education"
+            subTitle="National Inter College (2017 - 2019)"
+            result="74%"
+            des="Secondary education or post-primary education covers two phases on the International Standard Classification of Education scale."
+          />
+        </div>
+      </div>
+      {/* part Two */}
+      <div>
+        <div className="py-6 lgl:py-12 font-titleFont flex flex-col gap-4">
+          <p className="text-sm text-designColor tracking-[4px]">2024 - 2025</p>
+          <h2 className="text-3xl md:text-4xl font-bold">Job Experience</h2>
+        </div>
+        <div className="mt-6 lgl:mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Sr. Software Engineer"
+            subTitle="Nowgray It Services pvt Ltd Lko  - (2024- )"
+            result="USA"
+            des="Google's hiring process is an important part of our culture. Googlers care deeply about their teams and the people who make them up."
+          />
+          <ResumeCard
+            title="Web Developer & Trainer"
+            subTitle="Apple Developer Team - (2012 - 2016)"
+            result="MALAYSIA"
+            des="A popular destination with a growing number of highly qualified homegrown graduates, it's true that securing a role in Malaysia isn't easy."
+          />
+          <ResumeCard
+            title="Front-end Developer"
+            subTitle="Nike - (2020 - 2011)"
+            result="Oman"
+            des="The Oman economy has grown strongly over recent years, having transformed itself from a producer and innovation-based economy."
+          />
+        </div>
+        <div>gmail.com</div>
+      </div>
+    </motion.div>
+  );
+}
+export default Education;

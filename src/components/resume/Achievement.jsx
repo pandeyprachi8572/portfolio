98b7e362a39be8db4e80293f4a440e5f1f4e6d10
prachipandey <pandeyprachi8572@gmail.com>
2025-04-04 13:17:50 +0530

    *

--- src/components/resume/Achievement.jsx
+++ src/components/resume/Achievement.jsx
@@ -9,7 +9,7 @@ return (
         <h2 className="text-5xl font-bold  col-span-2 text-center mb-6">Achievements</h2>
       <div className="bg-gray-100 p-2 rounded-lg text-center">
         <h2 className="text-xl font-bold text-black mb-4">Internship Web Devlopment</h2>
-        <img src="/src/assets/images/Screenshot 2025-04-03 153030.png" alt="Achievement 1" className="w-full h-auto max-h-[300px] object-contain mb-4" />
+        <img src="/src/assets/images/projects/Screenshot 2025-04-03 153030.png" alt="Achievement 1" className="w-full h-auto max-h-[300px] object-contain mb-4" />
         <p className="text-black">Web Development Internship (5 months): Nowgray IT Services Pvt Ltd, Lucknow</p>
       </div>
       <div className="bg-gray-100 p-2 rounded-lg text-center">
@@ -24,7 +24,7 @@ return (
       </div>
       <div className="bg-gray-100 p-2 rounded-lg text-center">
         <h2 className="text-xl font-bold text-black mb-4">CSS Certificate </h2>
-        <img src="/src/assets/images/a664b4c1-4150-4d05-bfcd-de61231ccc5a.jpg" alt="Achievement 4" className="w-full h-auto max-h-[300px] object-contain mb-4" />
+        <img src="src/assets/images/a664b4c1-4150-4d05-bfcd-de61231ccc5a.jpg" alt="Achievement 4" className="w-full h-auto max-h-[300px] object-contain mb-4" />
         <p className="text-black">CSS Certificate by Digicoder .</p>
       </div>
     </motion.div>
--- src/components/resume/Resume.jsx
+++ src/components/resume/Resume.jsx
@@ -1,12 +1,9 @@
-
-
 import React, {  useState } from 'react'
 import Title from '../layouts/Title';
 import Education from './Education';
 import Skills from './Skills';
 import Achievement from './Achievement';
-import Experience from "./Experience"
-
+import Experience from "./Experience";
 const Resume = () => {
    const [educationData, setEducationData] = useState(true);
    const [skillData, setSkillData] = useState(false);
@@ -87,5 +84,4 @@ const Resume = () => {
     </section>
   );
 }
-
 export default Resume

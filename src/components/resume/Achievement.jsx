a982de26ee35251240e93896692720adb7b094f3
prachipandey <pandeyprachi8572@gmail.com>
2025-04-04 14:08:26 +0530

    *

--- src/components/resume/Achievement.jsx
+++ src/components/resume/Achievement.jsx
@@ -14,17 +14,17 @@ return (
       </div>
       <div className="bg-gray-100 p-2 rounded-lg text-center">
         <h2 className="text-xl font-bold text-black mb-4">React Certificate</h2>
-        <img src="/src/assets/images/Screenshot 2025-04-03 160519.png" alt="Achievement 2" className="w-full h-auto max-h-[300px] object-contain mb-4" />
+        <img src="/src/assets/images/projects/Screenshot 2025-04-03 160519.png" alt="Achievement 2" className="w-full h-auto max-h-[300px] object-contain mb-4" />
         <p className="text-black"> React Certificate by Hacker Rank</p>
       </div>
       <div className="bg-gray-100 p-2 rounded-lg text-center">
         <h2 className="text-xl font-bold text-black mb-4">JavaScript Cirtificate</h2>
-        <img src="/src/assets/images/download.png" alt="Achievement 3" className="w-full h-auto max-h-[300px] object-contain mb-4" />
+        <img src="/src/assets/images/projects/download.png" alt="Achievement 3" className="w-full h-auto max-h-[300px] object-contain mb-4" />
         <p className="text-black"> JavaScript Certificate by Hacker Rank.</p>
       </div>
       <div className="bg-gray-100 p-2 rounded-lg text-center">
         <h2 className="text-xl font-bold text-black mb-4">CSS Certificate </h2>
-        <img src="src/assets/images/a664b4c1-4150-4d05-bfcd-de61231ccc5a.jpg" alt="Achievement 4" className="w-full h-auto max-h-[300px] object-contain mb-4" />
+        <img src="src/assets/images/projects/a664b4c1-4150-4d05-bfcd-de61231ccc5a.jpg" alt="Achievement 4" className="w-full h-auto max-h-[300px] object-contain mb-4" />
         <p className="text-black">CSS Certificate by Digicoder .</p>
       </div>
     </motion.div>
